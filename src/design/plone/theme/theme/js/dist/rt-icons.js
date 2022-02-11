f529ec3a3b27ce11838680c9480b5988e1e1c1fc
alessandro.ceglie <alessandro.ceglie@uniba.it>
2022-02-11 16:21:23 +0100

    fix funzionalita' portal_tabs. rollover, menu' mobile, etc. TODO: fixare i colori

--- src/design/plone/theme/theme/js/dist/rt-icons.js
+++ src/design/plone/theme/theme/js/dist/rt-icons.js
@@ -76,8 +76,6 @@ define('fa_rt', [], function() {
     /******/ [
       /* 0 */
       /***/ function(module, exports, __webpack_require__) {
-        'use strict';
-
         Object.defineProperty(exports, '__esModule', {
           value: true,
         });
@@ -290,7 +288,6 @@ define('fa_rt', [], function() {
       },
       /* 1 */
       /***/ function(module, __webpack_exports__, __webpack_require__) {
-        'use strict';
         Object.defineProperty(__webpack_exports__, '__esModule', {
           value: true,
         });
@@ -1194,8 +1191,6 @@ define('fa_rt', [], function() {
 
         var perf = { begin: begin, end: end };
 
-        ('use strict');
-
         /**
          * Internal helper to bind a function known to have 4 arguments
          * to a given context.
@@ -1205,8 +1200,6 @@ define('fa_rt', [], function() {
             return func.call(thisContext, a, b, c, d);
           };
         };
-
-        ('use strict');
 
         /**
          * # Reduce
@@ -3213,7 +3206,6 @@ define('fa_rt', [], function() {
     ]
   );
 });
-
 require(['fa_rt'], function(fa_rt) {
   var init = fa_rt.init;
   var timeout = setTimeout(function() {
